fb479923c5b7c256c1c86b8719d8282f29bba927
Ragmaanir <ragmaanir@gmail.com>
2018-09-14 17:40:58 +0200

    LibGLFW window and surface

--- examples/basic/basic.cr
+++ examples/basic/basic.cr
@@ -1,3 +1,4 @@
+require "lib_glfw"
 require "../../src/kimberlite/libvulkan"
 
 class App
@@ -10,11 +11,16 @@ class App
   getter device : Vulkan::Device = nil.as(Vulkan::Device)
   getter graphics_queue : Vulkan::Queue = nil.as(Vulkan::Queue)
 
+  getter surface : Vulkan::SurfaceKhr = nil.as(Vulkan::SurfaceKhr)
+  getter window : Pointer(LibGLFW::Window) = nil.as(LibGLFW::Window*)
+
   def version(major : Int32, minor : Int32, patch : Int32)
     (major << 22) | (minor << 12) | patch
   end
 
   def initialize
+    LibGLFW.init
+
     puts "Extensions:"
     enumerate_extensions.each do |ext|
       puts ext.extension_name.map(&.unsafe_chr).join
@@ -25,6 +31,7 @@ class App
       puts layer.layer_name.map(&.unsafe_chr).join
     end
 
+    # -------------------- create instance
     app_info = Vulkan::ApplicationInfo.new
     app_info.s_type = Vulkan::StructureType::VkStructureTypeApplicationInfo
     app_info.p_application_name = "Hello Triangle".to_unsafe
@@ -37,16 +44,21 @@ class App
     create_info.s_type = Vulkan::StructureType::VkStructureTypeInstanceCreateInfo
     create_info.p_application_info = pointerof(app_info)
 
-    # uint32_t glfwExtensionCount = 0;
-    # const char** glfwExtensions;
+    exts = ["VK_EXT_debug_utils".to_unsafe]
 
-    # glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    glfw_req_count : UInt32 = 0
+    glfw_req = LibGLFW.get_required_instance_extensions(pointerof(glfw_req_count))
 
-    # createInfo.enabledExtensionCount = glfwExtensionCount;
-    # createInfo.ppEnabledExtensionNames = glfwExtensions;
+    puts "Required extensions by GLFW:"
+    glfw_req_count.times do |i|
+      puts "- #{String.new(glfw_req[i])}"
 
-    create_info.enabled_extension_count = 1
-    create_info.pp_enabled_extension_names = ["VK_EXT_debug_utils".to_unsafe].to_unsafe
+      exts << glfw_req[i]
+    end
+
+    create_info.enabled_extension_count = exts.size
+    create_info.pp_enabled_extension_names = exts.to_unsafe
+
     create_info.enabled_layer_count = 1
     create_info.pp_enabled_layer_names = ["VK_LAYER_LUNARG_standard_validation".to_unsafe].to_unsafe
 
@@ -56,8 +68,10 @@ class App
       raise "Create instance failed"
     end
 
+    # -------------------- debug callback
     register_debug_callback
 
+    # -------------------- enumerate devices, properties and queue families
     devices = enumerate_physical_devices
 
     puts "Physical Devices: #{devices.size}"
@@ -76,9 +90,21 @@ class App
 
     puts "Graphics Queue Index: #{graphics_queue_idx}"
 
+    # -------------------- create logical device
     @device = create_logical_device(graphics_queue_idx.to_u32)
 
     Vulkan.get_device_queue(device, graphics_queue_idx.to_u32, 0, pointerof(@graphics_queue))
+
+    # ---------- glfw window
+    LibGLFW.window_hint(LibGLFW::CLIENT_API, LibGLFW::NO_API)
+    @window = LibGLFW.create_window(800, 600, "vk".to_unsafe, nil, nil)
+
+    glfw_inst = Pointer(LibGLFW::VkInstance).new(instance.address)
+    glfw_surf = Pointer(LibGLFW::VkSurfaceKHR).new(pointerof(@surface).address)
+
+    LibGLFW.create_window_surface(glfw_inst, window, nil, glfw_surf)
+
+    # -------------------- destroy
 
     destroy
   end
@@ -95,6 +121,7 @@ class App
   end
 
   def destroy
+    Vulkan.destroy_surface_khr(instance, surface, nil)
     Vulkan.destroy_device(device, nil)
     destroy_debug_utils_messenger_ext(instance, @debug_callback.pointer.as(Vulkan::DebugUtilsMessengerExt), nil.as(Vulkan::AllocationCallbacks*))
     Vulkan.destroy_instance(instance, nil)
